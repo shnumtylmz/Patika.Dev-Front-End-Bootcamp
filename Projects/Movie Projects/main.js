fed6c70c0bf00cc880a41c55d5a08d0e13889cc6
Şahin Umut Yılmaz <shnumtylmz@gmail.com>
2024-08-11 16:21:55 +0300

    movie projects added

---
+++ Projects/Movie Projects/main.js
@@ -0,0 +1,247 @@
+const cardData = [
+{
+    title: "American Beauty",
+    description : "Lester Burnham, a depressed suburban father in a mid-life crisis, decides to turn his hectic life around after developing an infatuation with his daughter’s attractive friend.",
+    image: "./images/amerıcanbeauty.jpg",
+    type: "thriller"
+},
+{
+    title: "American Psyhco",
+    description : "A wealthy New York investment banking executive hides his alternate psychopathic ego from his co-workers and friends as he escalates deeper into his illogical, gratuitous fantasies.",
+    image: "./images/amerıcanpsyhco.jpg",
+    type: "thriller"
+},
+{
+    title: "Avatar",
+    description : "In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
+    image: "./images/avatar.jpg",
+    type: "science fiction"
+},
+{
+    title: "Avengers",
+    description : "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
+    image: "./images/avengars.jpg",
+    type: "science fiction"
+},
+{
+    title: "Barbie",
+    description : "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
+    image: "./images/barbie.jpg",
+    type: "science fiction"
+},
+{
+    title: "Blade Runner",
+    description : "Thirty years after the events of the first film, a new blade runner, LAPD Officer K, unearths a long-buried secret that has the potential to plunge what’s left of society into chaos. K’s discovery leads him on a quest to find Rick Deckard, a former LAPD blade runner who has been missing for 30 years.",
+    image: "./images/bladerunner.jpg",
+    type: "science fiction"
+},
+{
+    title: "Dead Poets Society",
+    description : "At an elite, old-fashioned boarding school in New England, a passionate English teacher inspires his students to rebel against convention and seize the potential of every day, courting the disdain of the stern headmaster.",
+    image: "./images/deadpoets.jpg",
+    type : "drama"
+},
+{
+    title: "Django Unchained",
+    description : "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.",
+    image: "./images/django.jpg",
+    type: "thriller"
+},
+{
+    title: "Don't Look Up",
+    description : "Two low-level astronomers, upon discovering that a meteor will strike the Earth in six months, go on a media tour to try to warn the world but find an unreceptive and unbelieving populace.",
+    image: "./images/dontlookup.jpg",
+    type: "comedy"
+},
+{
+    title: "The Godfather",
+    description : "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.",
+    image: "./images/godfather.jpg",
+    type: "thriller"
+},
+{
+    title: "Good Will Hunting",
+    description : "Will Hunting has a genius-level IQ but chooses to work as a janitor at MIT. When he solves a difficult graduate-level math problem, his talents are discovered by Professor Gerald Lambeau, who decides to help the misguided youth reach his potential. When Will is arrested for attacking a police officer, Professor Lambeau makes a deal to get leniency for him if he will get treatment from therapist Sean Maguire.",
+    image: "./images/goodwillhuntng.jpg",
+    type : "drama"
+},
+{
+    title: "Harry Potter",
+    description : "Year three at Hogwarts means new fun and challenges as Harry learns the delicate art of approaching a Hippogriff, transforming shape-shifting Boggarts into hilarity and even turning back time. But the term also brings danger: soul-sucking Dementors hover over the school, an ally of the accursed He-Who-Cannot-Be-Named lurks within the castle walls, and fearsome wizard Sirius Black escapes Azkaban. And Harry will confront them all.",
+    image: "./images/harrypotter.jpg",
+    type: "science fiction"
+},
+{
+    title: "Iron Man",
+    description : "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    image: "./images/ıronman.jpg",
+    type: "science fiction"
+},
+{
+    title: "Jojo Rabbit",
+    description : "A World War II satire that follows a lonely German boy whose world view is turned upside down when he discovers his single mother is hiding a young Jewish girl in their attic. Aided only by his idiotic imaginary friend, Adolf Hitler, Jojo must confront his blind nationalism.",
+    image: "./images/jojorabbit.jpg",
+    type : "drama"
+},
+{
+    title: "Killers Of The Flower Moon",
+    description : "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered one by one—until the FBI steps in to unravel the mystery.",
+    image: "./images/kıllersoftheflowermoon.jpg",
+    type: "thriller"
+},
+{
+    title: "Lord Of the Rings",
+    description : "Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.",
+    image: "./images/lordoftherıngs.jpg",
+    type: "science fiction"
+},
+{
+    title: "Madmax",
+    description : "Max Rockatansky returns as the heroic loner who drives the dusty roads of a postapocalyptic Australian Outback in an unending search for gasoline. Arrayed against him and the other scraggly defendants of a fuel-depot encampment are the bizarre warriors commanded by the charismatic Lord Humungus, a violent leader whose scruples are as barren as the surrounding landscape.",
+    image: "./images/madmax.jpg",
+    type: "thriller"
+},
+{
+    title: "Once Upon a Time in Hollywood",
+    description : "Los Angeles, 1969. TV star Rick Dalton, a struggling actor specializing in westerns, and stuntman Cliff Booth, his best friend, try to survive in a constantly changing movie industry. Dalton is the neighbor of the young and promising actress and model Sharon Tate, who has just married the prestigious Polish director Roman Polanski…",
+    image: "./images/onceuponhollywood.jpg",
+    type: "thriller"
+},
+{
+    title: "Oppenheimer",
+    description : "The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.",
+    image: "./images/oppenheimer.jpg",
+    type : "drama"
+},
+{
+    title: "Pulp Fiction",
+    description : "A burger-loving hit man, his philosophical partner, a drug-addled gangster’s moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.",
+    image: "./images/pulp-fiction.jpg",
+    type: "thriller"
+},
+{
+    title: "Shining",
+    description : "Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren’t prepared for the madness that lurks within.",
+    image: "./images/shining.jpg",
+    type: "thriller"
+},
+{
+    title: "Shrek 2",
+    description : "Shrek, Fiona and Donkey set off to Far, Far Away to meet Fiona’s mother and father. But not everyone is happy. Shrek and the King find it hard to get along, and there’s tension in the marriage. The fairy godmother discovers that Shrek has married Fiona instead of her Son Prince Charming and sets about destroying their marriage.",
+    image: "./images/shrek2.jpg",
+    type: "comedy"
+},
+{
+    title: "Shutter Island",
+    description : "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
+    image: "./images/shutterısland.jpg",
+    type: "thriller"
+},
+{
+    title: "Spiderman",
+    description : "After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.",
+    image: "./images/spıderman.jpg",
+    type: "science fiction"
+},
+{
+    title: "Starwars",
+    description : "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+    image: "./images/starwars.jpg",
+    type: "science fiction"
+},
+{
+    title: "Superbad",
+    description : "Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.",
+    image: "./images/superbad.jpg",
+    type: "comedy"
+},
+{
+    title: "The Truman Show",
+    description : "Truman Burbank is the star of The Truman Show, a 24-hour-a-day reality TV show that broadcasts every aspect of his life without his knowledge. His entire life has been an unending soap opera for consumption by the rest of the world. And everyone he knows, including his wife and his best friend is really an actor, paid to be part of his life.",
+    image: "./images/trumanshow.jpg",
+    type : "drama"
+},
+{
+    title: "Uncut Gems",
+    description : "A charismatic New York City jeweler always on the lookout for the next big score makes a series of high-stakes bets that could lead to the windfall of a lifetime. Howard must perform a precarious high-wire act, balancing business, family, and encroaching adversaries on all sides in his relentless pursuit of the ultimate win.",
+    image: "./images/uncutgems.jpg",
+    type: "thriller"
+},
+{
+    title: "Wonka",
+    description : "Willy Wonka – chock-full of ideas and determined to change the world one delectable bite at a time – is proof that the best things in life begin with a dream, and if you’re lucky enough to meet Willy Wonka, anything is possible.",
+    image: "./images/willywonka.jpg",
+    type: "comedy"
+},
+];
+
+const cardsContainer = document.getElementById("card-container");
+
+function renderCards(data){
+    cardsContainer.innerHTML = "";
+    data.forEach(data => {
+        const cardHTML = `
+        <div class="card mx-4 mt-2" style="width: 18rem;">
+                <img style="position: relative;" class="card-img-top" src="${data.image}" alt="Card image cap">
+                <i class="fa-regular fa-star card-img-top mt-2 star-icon" style="position: absolute"></i>
+                <i class="fa-solid fa-star card-img-top mt-2 star-icon" style="position: absolute"></i>
+                <div class="card-body">
+                    <h5 class="card-title">${data.title}</h5>
+                    <p class="card-text">${data.description}</p>
+                    <a href="#" class="btn btn-primary">Get Details</a>
+                </div>
+            </div>
+        `;
+        cardsContainer.innerHTML += cardHTML;
+    });
+}
+
+renderCards(cardData);
+
+document.getElementById('homeBtn').addEventListener("click", function(){
+    renderCards(cardData);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("drama").addEventListener('click', function(){
+    const dramaCards = cardData.filter(data => data.type === "drama");
+    renderCards(dramaCards);
+});
+
+document.getElementById("sci-fi").addEventListener('click', function(){
+    const scifiCards = cardData.filter(data => data.type === "science fiction");
+    renderCards(scifiCards);
+})
+
+document.getElementById("comedy").addEventListener('click', function(){
+    const comedyCards = cardData.filter(data => data.type === "comedy");
+    renderCards(comedyCards);
+});
+
+document.getElementById("thriller").addEventListener('click', function(){
+    const thrillerCards = cardData.filter(data => data.type === "thriller");
+    renderCards(thrillerCards);
+})
