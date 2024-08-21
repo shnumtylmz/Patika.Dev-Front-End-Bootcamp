ccce8209d41759844bc4420ccc4f8f53da68fb51
Şahin Umut Yılmaz <shnumtylmz@gmail.com>
2024-08-21 12:45:22 +0300

    live updated

--- Week-6/sports-center-react/vite.config.js
+++ Week-6/sports-center-react/vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/shnumtylmz/Patika.Dev-Front-End-Bootcamp/tree/main/Week-6/sports-center-react'
 });
